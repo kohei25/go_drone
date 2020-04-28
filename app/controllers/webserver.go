63d01d5e21df97b16d4c8c094f305639b2d1e8a6
kohei25 <nicodoteng@gmail.com>
2020-04-28 19:53:14 +0900

    config webserver

---
+++ app/controllers/webserver.go
@@ -0,0 +1,22 @@
+package controllers
+
+import (
+	"fmt"
+	"html/template"
+	"net/http"
+
+	"github.com/kohei25/go_drone.git/config"
+)
+
+func viewIndexHandler(w http.ResponseWriter, r *http.Request) {
+	t, _ := template.ParseFiles("app/views/index.html")
+	err, _ := t.Execute(w, nil)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+}
+
+func StartWebServer() error {
+	http.HandlerFunc("/", viewIndexHandler)
+	return http.ListenAndServe(fmt.Sprintf("%s:%d", config.Config.Address), nil)
+}
